47f0dc2e9297a1ad844de2590bad6151885992cb
Brian Bycynski <high5bri@hotmail.com>
2017-03-13 14:12:14 -0400

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <h1>Hello, Bdog</h1>
     </div>
   }
 }
