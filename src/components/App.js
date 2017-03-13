9d66cb43bbc1e4a7f11544ffad79b9848654a224
Brian Bycynski <high5bri@hotmail.com>
2017-03-13 14:16:53 -0400

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react'
 class App extends Component {
   render () {
     return <div>
-      <h1>Hello, Bdog</h1>
+      <div className='wrapper'>
+        <div className='letter'>
+        A
+      </div>
+        <div className='letter'>
+        B
+      </div>
+        <div className='letter'>
+        C
+      </div>
+        <div className='letter'>
+        D
+      </div>
+      </div>
+      <h1>Adjust the screen Size to see the responsiveness in the Grid!</h1>
     </div>
   }
 }
